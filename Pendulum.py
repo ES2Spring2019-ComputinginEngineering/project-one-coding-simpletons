42db44ee50a21df5f1df3cac6d6812708da6a609
ronangiss <ronan@sept.gmail.com>
2019-02-26 15:56:22 -0500

    Update Pendulum.py

--- Pendulum.py
+++ Pendulum.py
@@ -24,35 +24,34 @@ position = [(m.pi)/4]
 velocity = [0]
 acceleration = []
 
-time = np.linspace(0, 10, 10001)
-print(len(time))
-
-i = 0
+time = np.linspace(0, 10, 101)
+acceleration_repeat = [4,3,2,1,0,-1,-2,-3,-4,-3,-2,-1,0,1,2,3]
+acceleration = acceleration_repeat*6 + [4,3,2,1] 
+print(acceleration)
+i = 1
 while i < (len(time)-1):
-    #print_system(time[i],position[i],velocity[i])
-    if (len(time)%20 == 1):
-        acceleration.extend([4,3,2,1,0,0-1,-2,-3,-4,-4,-3,-2,-1,0,1,2,3,4])
-    positionf, velocityf = system_update(acceleration[i],position[i-1],velocity[i-1],time[i-1],time[i])
+    positionf = system_update(acceleration[i-1],position[i-1],velocity[i-1],time[i-1],time[i])[0]
+    velocityf = system_update(acceleration[i-1],position[i-1],velocity[i-1],time[i-1],time[i])[1]
     position.append(positionf)
     velocity.append(velocityf)
+    #print_system(time[i], position[i], velocity[i])
     i += 1
-    
-plt.plot(time, position, color = 'magenta')
+print(count)
+plt.plot(time[1:], position, color = 'magenta')
 plt.title('Position as a function of time', fontsize = 14)
 plt.xlabel('time', fontsize = 12)
 plt.ylabel('position', fontsize = 12)
 plt.show()
 
-plt.plot(time, velocity, color = 'cyan')
+plt.plot(time[1:], velocity, color = 'cyan')
 plt.title('Velocity as a function of time', fontsize = 14)
 plt.xlabel('time', fontsize = 12)
 plt.ylabel('velocity', fontsize = 12)
 plt.show()
 
-plt.plot(time, acceleration, color = 'yellow')
+plt.plot(time[1:], acceleration, color = 'orange')
 plt.title('Acceleration as a function of time', fontsize = 14)
 plt.xlabel('time', fontsize = 12)
 plt.ylabel('acceleration', fontsize = 12)
 plt.show()
-#putting on our Big boy pants
 
