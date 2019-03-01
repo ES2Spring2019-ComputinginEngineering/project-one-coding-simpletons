e2c8d3d031cb98b9d0719bcbc1b94e82027d3ebc
Ben Pradko <Benjamin.Pradko@tufts.edu>
2019-02-28 20:01:28 -0500

    Update data collection.py

---
+++ data collection.py
@@ -0,0 +1,22 @@
+#Ben Pradko and Ronan Gissler
+#This code is for collecting the acceleration data of the motion
+
+import math
+import microbit
+
+count = 0
+
+def tilt(x, y):
+    angle = math.atan2(y, x)
+    return angle
+
+fout = open('data_collection.txt', 'w')
+while (count < 1000):
+    x = microbit.accelerometer.get_x()
+    y = microbit.accelerometer.get_y()
+    angle = tilt(x, y)
+    fout.write(str(angle) + '\n')
+    microbit.sleep(10)
+    count += 1
+fout.close()
+
